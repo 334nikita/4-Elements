f51e00234c262331f7849ed58ff399df4ed8b1c6
DEADEYE <63458087+334nikita@users.noreply.github.com>
2023-07-06 17:50:47 +0300

    Add files via upload

--- Game/createfield.js
+++ Game/createfield.js
@@ -1,3 +1,34 @@
+import { createMenu } from "./createMenu.js";
+
 export const createField = (lvl) => {
+    const main = document.querySelector("main");
+    main.classList.add("null");
+
+
     console.log(lvl);
+    let lvlText;
+
+    switch (lvl) {
+        case 8:
+            lvlText = "Лёгкий";
+            break;
+        case 12:
+            lvlText = "Средний"
+            break;
+        case 16:
+            lvlText = "Сложный"
+            break;
+
+    }
+
+
+    const game = document.querySelector(".game");
+    game.style.display = "block";
+    const level = game.querySelector(".lvl")
+    level.textContent = lvlText;
+
+    const menu = game.querySelector(".btn")
+    menu.addEventListener('click', () => {
+        createMenu();
+    })
 }
